d698a0e5f49748331e2c2c035e0cd94ac8a9e44c
Matt Thorne <matt.thorne1982@gmail.com>
2022-07-31 23:34:25 +0100

    adding supported file type filters and program exit if nothing found

--- single_upload.py
+++ single_upload.py
@@ -7,26 +7,51 @@ from time import sleep
 siteId = key_file.MY_JW_SITE_ID
 apiV2Key = key_file.MY_V2_API_KEY
 
-category = [
+location = [
     inquirer.List(
         'option', 
-        message='Select the media category and press return', 
-        choices=['Automotive','Books and Literature','Business and Finance','Careers','Education','Events and Attractions','Family and Relationships','Fine Art','Food and Drink','Healthy Living','Hobbies and Interests','Home and Garden','Medical Health','Movies','Music and Audio','News and Politics','Personal Finance','Pets','Pop Culture','Real Estate','Religion and Spirituality','Science','Shopping','Sports','Style and Fashion','Technology and Computing','Television','Travel','Video Gaming'],
+        message='Are you files stored in the video folder? Y/N', 
+        choices=['Yes','No'],
         carousel=True
     ),
 ]
 
-print('Please enter the folder path containing video files')
+locationChoice = inquirer.prompt(location)
 
-filePath = input()
+locationChoiceAnswer = locationChoice.get('option')
+
+if locationChoiceAnswer == 'Yes':
+    filePath = key_file.VIDEO_FILE_PATH
+else:
+    print('Please enter the file path:')
+    filePath = input()
 
 dirList = listdir(filePath)
+
+videoList = []
+for fileName in dirList:
+    if fileName.endswith('.mp4'):
+        videoList.append(fileName)
+    if fileName.endswith('.m4a'):
+        videoList.append(fileName)
+    if fileName.endswith('.3gp'):
+        videoList.append(fileName)
+    if fileName.endswith('.avi'):
+        videoList.append(fileName)
+    if fileName.endswith('.mov'):
+        videoList.append(fileName)
+    if fileName.endswith('.wmv'):
+        videoList.append(fileName)
+
+if videoList == []:
+    print('No supported media in directory, program ending')
+    exit()
 
 mediaChoice = [
     inquirer.List(
         'option', 
         message = 'Select the media to upload', 
-        choices = dirList,
+        choices = videoList,
         carousel = True
     ),
 ]
@@ -40,7 +65,17 @@ meidaUrl = f'{filePath}{media}'
 print('Enter the media title and press return')
 mediaTitle = input()
 
+category = [
+    inquirer.List(
+        'option', 
+        message='Select the media category and press return', 
+        choices=['Automotive','Books and Literature','Business and Finance','Careers','Education','Events and Attractions','Family and Relationships','Fine Art','Food and Drink','Healthy Living','Hobbies and Interests','Home and Garden','Medical Health','Movies','Music and Audio','News and Politics','Personal Finance','Pets','Pop Culture','Real Estate','Religion and Spirituality','Science','Shopping','Sports','Style and Fashion','Technology and Computing','Television','Travel','Video Gaming'],
+        carousel=True
+    ),
+]
+
 choice = inquirer.prompt(category)
+
 mediaCategory = choice.get('option')
 
 url = f'https://api.jwplayer.com/v2/sites/{siteId}/media'
